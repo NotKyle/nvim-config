da2fad121729717d141a988082c1df40928de32c
Kyle Russell <NotKyle@users.noreply.github.com>
2024-01-15 11:37:36 +0000

    Config changes

--- lua/plugins/kylerussell.lua
+++ lua/plugins/kylerussell.lua
@@ -5,4 +5,32 @@ return {
 			colorScheme = "morning",
 		},
 	},
+
+  {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+  },
+
+  {
+    "pocco81/auto-save.nvim",
+    opts = {
+      enabled = true,
+    }
+  },
+
+  {
+    "ahmedkhalf/project.nvim",
+    opts = {
+      manual_mode = false,
+      projects = {
+        ["/Users/kylerussell/Desktop/Sites/"] = {
+          ["*.lua"] = { "nvim" },
+          ["*.go"] = { "go" },
+          ["*.py"] = { "python" },
+          ["*.php"] = { "php" },
+        },
+      },
+    }
+  },
 }
