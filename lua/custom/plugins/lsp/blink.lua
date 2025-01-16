f56536fa841699e1287f909e7e43bb58ea2bea6d
Kyle Russell <NotKyle@users.noreply.github.com>
2025-01-16 16:34:40 +0000

    Changes

--- lua/custom/plugins/lsp/blink.lua
+++ lua/custom/plugins/lsp/blink.lua
@@ -17,8 +17,52 @@ return {
     -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
     -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
     -- See the full "keymap" documentation for information on defining your own keymap.
-    keymap = { preset = 'default' },
-
+    keymap = {
+      preset = 'default',
+      ['<Up>'] = { 'select_prev', 'fallback' },
+      ['<Down>'] = { 'select_next', 'fallback' },
+      ['<C-space>'] = {
+        function(cmp)
+          cmp.show { 'lsp', 'path', 'snippets', 'buffer' }
+        end,
+      },
+      ['<Enter>'] = { 'accept', 'fallback' },
+      ['<Tab>'] = { 'accept', 'fallback' },
+      ['<S-Tab>'] = { 'select_prev', 'fallback' },
+    },
+    completion = {
+      keyword = { range = 'full' },
+      trigger = {
+        show_on_keyword = true,
+      },
+      ghost_text = {
+        enabled = false,
+      },
+      menu = {
+        auto_show = true,
+        -- auto_show = function(ctx)
+        --   return ctx.mode ~= 'cmdline' --or not vim.tbl_contains({ '/', '?' }, vim.fn.getcmdtype())
+        -- end,
+        draw = {
+          columns = {
+            { 'label', 'label_description', gap = 1 },
+            { 'kind_icon', 'kind' },
+          },
+        },
+      },
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 500,
+      },
+      list = {
+        selection = {
+          auto_insert = true,
+          preselect = function(ctx)
+            return ctx.mode ~= 'cmdline' and not require('blink.cmp').snippet_active { direction = 1 }
+          end,
+        },
+      },
+    },
     appearance = {
       -- Sets the fallback highlight groups to nvim-cmp's highlight groups
       -- Useful for when your theme doesn't support blink.cmp
@@ -28,7 +72,6 @@ return {
       -- Adjusts spacing to ensure icons are aligned
       nerd_font_variant = 'mono',
     },
-
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
@@ -36,4 +79,5 @@ return {
     },
   },
   opts_extend = { 'sources.default' },
+  signature = { enabled = true },
 }
