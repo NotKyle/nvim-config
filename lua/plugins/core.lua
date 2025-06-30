7e411465271be4e467df2664263a8d8dca4a02a6
Kyle Russell <kyle@jaskcreative.co.uk>
2025-06-30 16:00:13 +0100

    Changess

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -549,6 +549,12 @@ return {
   },
   {
     'f-person/git-blame.nvim',
+    opts = {
+      enabled = false, -- If you want to enable the plugin
+      message_template = ' <summary> • <date> • <author> • <<sha>>', -- Template for the blame message, check the Message template section for more options
+      date_format = '%m-%d-%Y %H:%M:%S', -- Template for the date, check Date format section for more options
+      virtual_text_column = 1, -- Virtual text start column, check Start virtual text at column section for more options
+    },
   },
   {
     'folke/drop.nvim',
