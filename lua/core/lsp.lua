a2ebce497d80f13f30a935fd73d7181fee3d9ac5
Kyle Russell <kyle@jaskcreative.co.uk>
2025-09-23 15:15:09 +0100

    Lsp change

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -7,9 +7,14 @@ vim.api.nvim_create_autocmd("FileType", {
 			php = "intelephense",
 			html = "html",
 			css = "cssls",
+			harper = "harper",
+			tailwindcss = "tailwindcss",
 		}
 		local server = map[ft]
 		if server then
+			-- Log the message using Noice but don't show it to the user
+			local Noice = require("noice")
+			Noice.log.info("Enabling LSP for " .. ft .. " using " .. server)
 			-- print("Enabling LSP for " .. ft .. " using " .. server)
 			vim.lsp.enable(server)
 		end
